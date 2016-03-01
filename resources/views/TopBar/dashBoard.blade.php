44f06a0a5bacb845f634768d9bbadbf818afd06f
aidan <aidandbush@gmail.com>
2016-03-01 14:45:40 -0700

    add tasks to dashboard page

--- resources/views/TopBar/dashBoard.blade.php
+++ resources/views/TopBar/dashBoard.blade.php
@@ -4,5 +4,13 @@
 <div class="container">
     <!--TODO work on dashboard -->
     This is where our dashBoard page goes!
+    <div class="tasksTable">
+        <h2>My Tasks</h2>
+        <ul>
+            @foreach($tasks as $task)
+                <li>{{$task->description}}</li>
+            @endforeach
+        </ul>
+    </div>
 </div>
 @stop
