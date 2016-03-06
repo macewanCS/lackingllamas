38412390927550f38773125a6cd3bb34ed234b95
Kevin Saforo <saforok@mymacewan.ca>
2016-03-05 20:57:16 -0500

    Format dashboard css, add logged in css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,15 +57,13 @@
                         <a href="{{ url('/register') }}">Register</a>
                     </div>
                 @else
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
+                    <button class="dropbtn">
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </button>
+                    <div class="dropdown-content">
+                        <a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a>
+                    </div>
 
-                        <ul class="dropdown-menu" role="menu">
-                            <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
-                        </ul>
-                    </li>
                 @endif
             </ul>
         </div>
