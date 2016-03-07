cb09b2e603cffe03ba0bc6850129ccac66b6a149
aidan <aidandbush@gmail.com>
2016-03-06 21:02:08 -0700

    fix home html

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
+                <div class="panel-heading">Home</div>
 
                 <div class="panel-body">
-                    You are logged in!
+                    You are not logged in
                 </div>
             </div>
         </div>
