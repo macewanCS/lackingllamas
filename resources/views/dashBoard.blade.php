99fb6ce8a87175eed26872e1970b8d38ddf2cfc8
Kevin Saforo <saforok@mymacewan.ca>
2016-03-08 00:51:22 -0500

    Fix topbar & dashboard scaling, add dashboard section

--- resources/views/dashBoard.blade.php
+++ resources/views/dashBoard.blade.php
@@ -5,37 +5,47 @@
     <div class="container-dashboard">
         <!--TODO work on dashboard -->
         <div class="inner">
+            <div class="welcomeTable">
+                <h2>Welcome, {{ Auth::user()->name }}</h2>
+                <div class="welcomeDescription">
+                </div>
+            </div>
             <div class="tasksTable">
-                <h2>My Tasks</h2>
-                <ul>
-                    @foreach($tasks as $task)
-                        <li>
-                            <div class="taskDescription">{{$task->description}}</div>
-                            <div class="taskDate">{{ \Carbon\Carbon::parse($task->date)->diffForHumans()}}</div>
-                        </li>
-                    @endforeach
-                </ul>
-                <div class="tasksBtn"><a href="{{ url('/mytasks') }}">View All</a></div>
+                <div class = "tasks-inner">
+                    <h2>My Tasks</h2>
+                    <ul>
+                        @foreach($tasks as $task)
+                            <li>
+                                <div class="taskDescription">{{$task->description}}</div>
+                                <div class="taskDate">{{ \Carbon\Carbon::parse($task->date)->diffForHumans()}}</div>
+                            </li>
+                        @endforeach
+                    </ul>
+                    <div class="tasksBtn"><a href="{{ url('/mytasks') }}">View All</a></div>
+                </div>
 
 
             </div>
 
             <div class="teamsTable">
-                <h2>My Teams & Departments</h2>
-                <ul>
-                    <!-- TODO Add teams & departments dynamically to the dashboard -->
-                    <li>IT Services Department</li>
-                    <li>Events Team</li>
-                </ul>
-
+                <div class="teams-inner">
+                    <h2>My Teams & Departments</h2>
+                    <ul>
+                        <!-- TODO Add teams & departments dynamically to the dashboard -->
+                        <li>IT Services Department</li>
+                        <li>Events Team</li>
+                    </ul>
+                </div>
             </div>
             <div class="notificationsTable">
-                <h2>Notifications</h2>
-                <ul>
-                    <li>'2016 Business Plan' was changed</li>
-                    <li>A task was changed</li>
-                </ul>
-                <div class="notificationsBtn"><a href="{{ url('/notifications') }}">View All</a></div>
+                <div class="notifications-inner">
+                    <h2>Notifications</h2>
+                    <ul>
+                        <li>'2016 Business Plan' was changed</li>
+                        <li>A task was changed</li>
+                    </ul>
+                    <div class="notificationsBtn"><a href="{{ url('/notifications') }}">View All</a></div>
+                </div>
             </div>
         </div>
     </div>
