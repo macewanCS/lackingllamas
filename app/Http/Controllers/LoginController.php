79db36e34062741e0e95eac45e50b73eadd10b93
aidan <aidandbush@gmail.com>
2016-03-22 16:23:31 -0600

    remove debug messages in login controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -13,10 +13,8 @@ class LoginController extends Controller
     public function login()
     {
         if (Auth::guest()){
-            Debugbar::info('if guest');
             return view('auth/login');
         } else {
-            Debugbar::info('else');
             return redirect('businessplan');
         }
     }
