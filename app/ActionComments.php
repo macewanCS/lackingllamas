a9992f11fb7241dc75b8c28bb6f27157aead65cf
Kevin Saforo <saforok@mymacewan.ca>
2016-03-22 17:37:51 -0400

    add migration and model for action comments

---
+++ app/ActionComments.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ActionComments extends Model
+{
+    protected $fillable = [
+        'description',
+        'date',
+        'user_ID',
+        'action_ID'
+    ];
+
+    public function getComments($action_id)
+    {
+        $comments = ActionComments::All();
+        $comment = $comments->where('action_ID', $action_id)->all();
+        return $comment;
+    }
+}
