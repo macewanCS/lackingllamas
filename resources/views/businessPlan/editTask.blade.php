986dedcfc4af979e3778452bd18d6a6ff8020c5d
Kevin Saforo <saforok@mymacewan.ca>
2016-03-23 01:46:08 -0400

    Create consistent css for editGOAT pages

--- resources/views/businessPlan/editTask.blade.php
+++ resources/views/businessPlan/editTask.blade.php
@@ -3,69 +3,73 @@
 
 @section('content')
 
-      {!! Html::style('css/createGoal.css') !!}
-      <div class="create-action-container">
-            <div class="create-goal-inner">
-            <h2>
-                  <link rel="stylesheet" href="//code.jquery.com/ui/1.11.2/themes/smoothness/jquery-ui.css">
-                  Edit a Task
-            </h2>
+    {!! Html::style('css/createGoal.css') !!}
+    <div class="create-action-container">
+        <h1>
+            Edit a Task
+        </h1>
+        <hr>
+        <div class="create-goal-inner">
 
+            {!! Form::model($task,['method' => 'PATCH', 'action' => ['BusinessPlanController@update', $task->id]]) !!}
+            <div class="form-group-one">
 
-            
-     
-             {!! Form::model($task,['method' => 'PATCH', 'action' => ['BusinessPlanController@update', $task->id]]) !!}
-                  <div class="form-group-one">
+                {!! Form::label('action_id','Action: ', ['class' => 'edit-task-label']) !!}
+                {!! Form::select('action_id',$actions,null, array('class' => 'form-extras')) !!}<br>
 
-                  {!! Form::label('action_id','Action: ') !!}<br>
-                  {!! Form::select('action_id',$actions,null, array('class' => 'form-control'))!!}<br>
+                <br>
+                {!! Form::label('description','Name: ', ['class' => 'edit-task-label']) !!}
+                {!! Form::text('description', null, ['class' => 'edit-action-field']) !!}<br>
 
-                
-                  {!! Form::label('description','Name: ') !!}<br>
-                  {!! Form::text('description') !!}<br>
+                <br>
+                {!! Form::label('date','Date: ', ['class' => 'edit-task-label']) !!}
+                {!! Form::input('date','date', date('Y-m-d'), ['class' => 'form-extras']) !!}<br>
 
-                  {!! Form::label('date','date: ') !!}<br>
-                  {!! Form::input('date','date', date('Y-m-d'), ['class' => 'form-control']) !!}<br>
+                <br>
+                {!! Form::label('leads','Leads: ', ['class' => 'edit-task-label']) !!}
+                {!! Form::text('leads', null, ['class' => 'edit-action-field']) !!}<br>
 
-                  {!! Form::label('leads','Leads: ') !!}<br>
-                  {!! Form::text('leads') !!}<br>
+                <br>
+                {!! Form::label('collaborators','Collaborators: ', ['class' => 'edit-task-label']) !!}
+                {!! Form::text('collaborators', null, ['class' => 'edit-action-field']) !!}<br>
 
-                  {!! Form::label('collaborators','Collaborators: ') !!}<br>
-                  {!! Form::text('collaborators') !!}<br>
+                <br>
+                {!! Form::label('budget','Budget: ', ['class' => 'edit-task-label']) !!}
+                {!! Form::text('budget', null, ['class' => 'edit-action-field']) !!}       <br>
 
-                  {!! Form::label('budget','budget: ') !!}<br>
-                  {!! Form::text('budget') !!}       <br>
-                           
-                  {!! Form::label('projectPlanprojectPlan','projectPlan: ') !!}<br>
-                  {!! Form::text('projectPlan') !!}  <br>
+                <br>
+                {!! Form::label('projectPlan','Project Plan: ', ['class' => 'edit-task-label']) !!}
+                {!! Form::text('projectPlan', null, ['class' => 'edit-action-field']) !!}  <br>
 
-                  {!! Form::label('successMeasured','successMeasured: ') !!}<br>
-                  {!! Form::text('successMeasured') !!} <br>
+                <br>
+                {!! Form::label('successMeasured','Success Measured: ', ['class' => 'edit-task-label']) !!}
+                {!! Form::text('successMeasured', null, ['class' => 'edit-action-field']) !!} <br>
 
-                  {!! Form::label('priority','priority: ') !!}<br>
-                  {!! Form::text('priority') !!} <br>
-                  {!! Form::hidden('teamOrDeptId', 1) !!}
-    <div id ="divBP">
-    {!! Form::label('bp','BusinessPlan: ') !!}
-    {!! Form::checkbox('bp', 1, true) !!}
-    <p class="text">Part of Business Plan? Checked means yes</p>
-    </div>
+                <br>
+                {!! Form::label('priority','Priority: ', ['class' => 'edit-task-label']) !!}
+                {!! Form::text('priority', null, ['class' => 'edit-action-field']) !!} <br>
+                {!! Form::hidden('teamOrDeptId', 1) !!}
+                <div id ="divBP">
+                    {!! Form::label('bp','Business Plan: ') !!}
+                    {!! Form::checkbox('bp', 1, true) !!}
+                    <p class="text">Part of Business Plan? Checked means yes</p>
+                </div>
             </div>
 
-          
-                  {!! Form::submit('Add Task',['class'=>'btn-primary-action form-control','data-toggle' => 'tooltip']) !!}
-           
 
-            
+            {!! Form::submit('Add Task',['class'=>'btn-primary-action form-control','data-toggle' => 'tooltip']) !!}
+
+
+
 
             {!! Form::close() !!}
-            </div>
-      </div>
+        </div>
+    </div>
 @stop
 <script src="//code.jquery.com/jquery-1.10.2.js"></script>
-  <script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
-  <script>
-  $(function() {
-    $( "#datepicker" ).datepicker();
-  });
-  </script>
+<script src="//code.jquery.com/ui/1.11.2/jquery-ui.js"></script>
+<script>
+    $(function() {
+        $( "#datepicker" ).datepicker();
+    });
+</script>
