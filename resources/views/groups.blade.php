624794a0eb7ec5ef7f85e9970e2f5d475b58433c
Kevin Saforo <saforok@mymacewan.ca>
2016-03-27 03:05:35 -0400

    Add basic div layout

--- resources/views/groups.blade.php
+++ resources/views/groups.blade.php
@@ -1,5 +1,22 @@
 @extends('layouts.app')
-
+@section('endHead')
+    {!! Html::style('css/groups.css') !!}
+@stop
 @section('content')
-    Test
+    <div class="groups-container">
+        <div class="groups-container-inner">
+            <div class="options-container">
+
+            </div>
+
+            <div class="roster-container">
+
+            </div>
+
+            <div class="group-details">
+
+            </div>
+        </div>
+
+    </div>
 @stop
