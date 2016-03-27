bb10ff29444850471a93244e558e449796c4e625
Kevin Saforo <saforok@mymacewan.ca>
2016-03-27 04:09:35 -0400

    Add more html for groups page

--- app/Http/Controllers/GroupController.php
+++ app/Http/Controllers/GroupController.php
@@ -3,12 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Group;
+use App\Action;
+use App\User;
 use App\Http\Requests;
 
 class GroupController extends Controller
 {
+    public function __construct() {}
+
     public function groups() {
-        return view('groups');
+        $groups = Group::all();
+        $actions = Action::all();
+        $users = User::all();
+        return view('groups', compact('groups', 'actions', 'users'));
     }
 }
