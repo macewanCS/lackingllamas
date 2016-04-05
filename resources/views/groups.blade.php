3217646f8a98252bb46417bbc87faf0e3a1b99a6
EdwardsNick <edwardsn8@mymacewan.ca>
2016-04-05 01:55:45 -0600

    Pizzazz the groups page

--- resources/views/groups.blade.php
+++ resources/views/groups.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('endHead')
     {!! Html::style('css/groups.css') !!}
+    {!! html::style('css/actionComments.css') !!}
 @stop
 @section('content')
     <div class="groups-container">
@@ -12,7 +13,7 @@
                         <div id="select-group">Departments</div>
                         @foreach($groups as $group)<!--departments-->
                             @if(!$group->team)
-                            <li class="result-list-elem team href="#" onclick="display('{{$group->name}}','{{$users->find($group->user_ID)->name}}' ,'{{$group->description}}', '{{$group->budget}}', '{{json_encode($actions[0])}}', '{{json_encode($tasks[0])}}', '{{json_encode($users)}}', '{{$group->id}}', '{{$rosters}}');return false;"">
+                            <li class="result-list-elem team" href="#" onclick="display('{{$group->name}}','{{$users->find($group->user_ID)->name}}' ,'{{$group->description}}', '{{$group->budget}}', '{{$group->id}}')">
                                 <a id="link-result">{{$group->name}}</a>
                             </li>
                             @endif
@@ -20,7 +21,7 @@
                         <div id="select-group">Teams</div>
                         @foreach($groups as $group)<!--teams-->
                             @if($group->team)
-                            <li class="result-list-elem department href="#" onclick="display('{{$group->name}}','{{$users->find($group->user_ID)->name}}' ,'{{$group->description}}', '{{$group->budget}}', '{{json_encode($actions[0])}}', '{{json_encode($tasks[0])}}', '{{json_encode($users)}}', '{{$group->id}}', '{{$rosters}}');return false;"">
+                            <li class="result-list-elem department" href="#" onclick="display('{{$group->name}}','{{$users->find($group->user_ID)->name}}' ,'{{$group->description}}', '{{$group->budget}}', '{{$group->id}}')">
                                 <a id="link-result">{{$group->name}}</a>
                             </li>
                             @endif
@@ -78,12 +79,17 @@
                     @endif
                 </div>
                     <br>
+
+            </div>
+            <div id="Rosters">
                 <h2 class="roster-headers"><label id="roster-colour-bar"></label>Roster</h2>
                 <div id="group-users">
                     @if (count($groups))
                         @foreach ($rosters as $roster)
                             @if ($roster->group_ID == $groups[0]['id'])
-                                <div class="roster-names">{{$users->find($roster->user_ID)->name}}</div>
+                                <div class="roster-names">
+                                    <a id="roster-link" href="{{url('/businessplan/' . $bpid . '/user/' . $users->find($roster->user_ID)->id)}}">{{$users->find($roster->user_ID)->name}}</a>
+                                </div>
                             @endif
                         @endforeach
                     @else
@@ -91,16 +97,14 @@
                     @endif
                 </div>
             </div>
-
-        </div>
-        <div class="business-plan-div">
-            {{Form::label('businessplan-label', 'Business Plan', ['class' => 'business-plan-selector'])}}
-            {{Form::select('business-plan', $businessPlans, null, ['class' => 'business-plan-selector'])}}
         </div>
     </div>
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+
+@stop
+
+@section('scripts')
     <script type="text/javascript">
-        function display(element, lead, description, budget, actions, tasks, users, id, rosters) {
+        function display(element, lead, description, budget, id) {
 
             var $name = element;
             var headerText = document.getElementById("group-name");
@@ -110,10 +114,10 @@
             var actionText = document.getElementById("group-actions");
             var taskText = document.getElementById("group-tasks");
             var userText = document.getElementById("group-users");
-            var actionsArray = JSON.parse(actions);
-            var tasksArray = JSON.parse(tasks);
-            var usersArray = JSON.parse(users);
-            var rostersArray = JSON.parse(rosters);
+            var actionsArray = $.parseJSON('{{json_encode($actions[$bpid-1], JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP)}}'.replace(/&quot;/g, '\u0022'));
+            var tasksArray = $.parseJSON('{{json_encode($tasks[$bpid-1], JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP)}}'.replace(/&quot;/g, '\u0022'));
+            var usersArray = $.parseJSON('{{json_encode($users, JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP)}}'.replace(/&quot;/g, '\u0022'));
+            var rostersArray = $.parseJSON('{{json_encode($rosters, JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP)}}'.replace(/&quot;/g, '\u0022'));
             var actionContent = '';
             var taskContent = '';
             var userContent = '';
