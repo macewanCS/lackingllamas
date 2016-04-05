961af62e39d728c768873a9a66477768e20e0825
Kevin Saforo <saforok@mymacewan.ca>
2016-04-04 23:26:08 -0400

    Add action comments seeder

---
+++ database/seeds/ActionCommentsSeeder.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class ActionCommentsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('action_comments')->insert([
+            ['description' => 'Waiting for the go ahead from J McPhee before we begin',
+                'date' => Carbon::createFromDate(2016, 1, 4, 'America/Toronto'),
+                'user_ID' => 4,
+                'action_ID' => 10
+            ],
+            ['description' => 'OK - We are good to go',
+                'date' => Carbon::createFromDate(2016, 1, 17, 'America/Toronto'),
+                'user_ID' => 2,
+                'action_ID' => 10
+            ],
+            ['description' => 'I got responses from Tai Lopez, Elon Musk, Ken Robinson, and Susan Cain. Any Preferences?',
+                'date' => Carbon::createFromDate(2016, 2, 4, 'America/Toronto'),
+                'user_ID' => 5,
+                'action_ID' => 9
+            ],
+            ['description' => 'Tai Lopez, easily',
+                'date' => Carbon::createFromDate(2016, 2, 6, 'America/Toronto'),
+                'user_ID' => 2,
+                'action_ID' => 9
+            ]
+        ]);
+    }
+}
