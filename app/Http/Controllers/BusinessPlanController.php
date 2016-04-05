ef58917c014b00ea9db18be74bac71cb584cfa2f
Nicholas Edwards <edwardsn8@mymacewan.ca>
2016-04-05 11:01:26 -0600

    Fix create GOAT bug

--- app/Http/Controllers/BusinessPlanController.php
+++ app/Http/Controllers/BusinessPlanController.php
@@ -399,14 +399,26 @@ class BusinessPlanController extends Controller
                    $collabs = "";
         if (array_key_exists('collaborators-groups', $input)) {
             foreach ($input['collaborators-groups'] as $x) {
-                $collabs .= Group::find($x)->name;
-                $collabs .= ", ";
+                $group = Group::find($x);
+                if ($group != null) {
+                    $collabs .= Group::find($x)->name;
+                    $collabs .= ", ";
+                }
+                else {
+                    $collabs .= "";
+                }
             }
         }
         if (array_key_exists('collaborators-users', $input)) {
             foreach ($input['collaborators-users'] as $x) {
-                $collabs .= User::find($x)->name;
-                $collabs .= ", ";
+                $user = User::find($x);
+                if ($user != null){
+                    $collabs .= User::find($x)->name;
+                    $collabs .= ", ";
+                }
+                else {
+                    $collabs .= "";
+                }
             }
         }
         $collabs = rtrim($collabs, ", ");
